26b0a7b1ba6ead53bd06557ceb203ba3e0cfcf01
Gjlang <gilangeko70@gmail.com>
2025-09-04 18:03:33 +0800

    d

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -279,7 +279,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
                             </svg>
                         </div>
-                        <h4 class="font-semibold text-lg mb-1 text-white">KLTG Jobs</h4>
+                        <h4 class="font-semibold text-lg mb-1 text-white">KLTG Monthly Jobs</h4>
                         <p class="text-sm text-white/90 text-center">Manage KLTG monthly jobs and tasks</p>
                     </a>
 
@@ -291,7 +291,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"/>
                             </svg>
                         </div>
-                        <h4 class="font-semibold text-lg mb-1 text-white">Media Jobs</h4>
+                        <h4 class="font-semibold text-lg mb-1 text-white">Social Media Monthly Jobs</h4>
                         <p class="text-sm text-white/90 text-center">Handle media monthly jobs and campaigns</p>
                     </a>
 
@@ -303,7 +303,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064"/>
                             </svg>
                         </div>
-                        <h4 class="font-semibold text-lg mb-1 text-white">Outdoor Jobs</h4>
+                        <h4 class="font-semibold text-lg mb-1 text-white">Outdoor Monthly Jobs</h4>
                         <p class="text-sm text-white/90 text-center">Manage outdoor monthly advertising jobs</p>
                     </a>
                 </div>
